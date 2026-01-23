424b4567cd2990b40595a4eeec6b61b174ea9953
EthanLev <ethancolemanlevesque@gmail.com>
2026-01-23 12:03:01 -0700

    Hello World

---
+++ ClassWork/HelloWorld/out/build/CMakePresets.json/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ethan/CSC2034-ELevesque/ClassWork/HelloWorld
+# Build directory: C:/Users/ethan/CSC2034-ELevesque/ClassWork/HelloWorld/out/build/CMakePresets.json
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
